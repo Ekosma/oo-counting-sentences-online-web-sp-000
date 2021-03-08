a8e3bc8328da38903096265061e2e70dbdbc6547
Evelyn Kosma <ezranaja@gmail.com>
2021-03-08 23:11:08 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,7 +16,7 @@ class String
 
   def count_sentences
     if self.end_with(".")
-      self.split
+      self.split(" ")
     elsif self.end_with("?")
       self.split
     else self.end_with("!")
